97bfaed2c16519ca16b4c621f96d890c46eca85b
gauravkulkarni96 <gauravkulkarni96@gmail.com>
2018-03-25 02:24:53 +0530

    templates relocate

--- ajax_auth/settings.py
+++ ajax_auth/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ajax_auth.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'registration', 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
